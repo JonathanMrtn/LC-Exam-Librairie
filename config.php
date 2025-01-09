16b19684e6552ca57ad4467522b26a3f7e53d08a
Jonathan <jojocraft42@gmail.com>
2025-01-09 12:34:24 +0100

    🧪 ✅ 🔐  ADD dependencies for security and unittest

--- config.php
+++ config.php
@@ -3,11 +3,15 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$host = 'localhost';
-$dbname = 'librairie';
-$username = 'root';
-$password = '';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
